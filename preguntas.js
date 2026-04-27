4b4ddaec47a90c8918a75da7c748eb40fd8e75fb
crizcar <149846473+crizcar@users.noreply.github.com>
2026-04-27 09:39:47 -0400

    Create preguntas.js

---
+++ preguntas.js
@@ -0,0 +1,40 @@
+// --- ARCHIVO: preguntas.js ---
+// Aquí centralizamos la información. Para las siguientes partes del certamen,
+// solo iremos agregando más objetos dentro de este arreglo principal.
+
+const preguntas = [
+    // --- PARTE 1 (Preguntas 1 a 3 del PDF) ---
+    {
+        pregunta: "¿Cuál de las siguientes expresiones, debiera ser el punto de partida de la gestión estratégica de RRHH?",
+        opciones: [
+            "Búsqueda de la Cultura", 
+            "Definición de la Estrategia", 
+            "Definición de la Visión", 
+            "Respuesta al Propósito de la empresa", 
+            "Declaración de la Misión"
+        ],
+        respuestaCorrecta: 4 // El índice empieza en 0. (0, 1, 2, 3, 4 = Declaración de la Misión)
+    },
+    {
+        pregunta: "¿Cuándo se puede decir que una empresa tiene un Modelo de Gestión de RRHH?",
+        opciones: [
+            "Cuando cada uno de sus procesos se realiza con excelencia y con los mejores resultados.",
+            "Cuando realiza los procesos de RRHH asociados al trabajador.",
+            "Cuando define en un esquema conceptual, cuál será la estrategia de RRHH para estrategia del negocio.",
+            "Cuando tiene todo documentado en procedimientos que definen el que hacer",
+            "Cuando define una estrategia para alcanzar los mejores resultados de las personas."
+        ],
+        respuestaCorrecta: 4
+    },
+    {
+        pregunta: "Según lo revisado en Clases, cual aseveración NO es correcta.",
+        opciones: [
+            "Las funciones Operativas, son aquellas que no se pueden dejar de realizar",
+            "Las funciones tácticas, están de apoyo a las operativas y pensadas en mediano plazo",
+            "Las funciones estratégicas, son las que hacen crecer al negocio.",
+            "Las funciones transaccionales de RRHH, son las que están documentadas en un procedimiento"
+        ],
+        respuestaCorrecta: 3
+    }
+    // Las futuras preguntas irán pegadas justo aquí debajo.
+];
